d1e29a87c767cfbbfb37101b552a616dc779d8d4
alimemco <alimoghadam75@gmail.com>
2018-10-29 10:50:58 +0330

    MUSIC PLAYER added

---
+++ app/src/main/java/com/ali/memco/evolaptop/view/activity/MusicPlayer.java
@@ -0,0 +1,15 @@
+package com.ali.memco.evolation.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ali.memco.evolation.R;
+
+public class MusicPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_player);
+    }
+}
